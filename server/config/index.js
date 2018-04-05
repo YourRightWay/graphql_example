3cc967f6356bb3b75e07ed28decb867b428c9f11
hnaumenko <hnaumenko@lohika.com>
2018-04-05 18:30:02 +0300

    Add mongo db

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 export default {
   port: 8000,
   host: 'localhost',
-  db: process.env.MONGO_URL || 'mongodb://localhost:27017/amwest-db',
+  db: process.env.MONGO_URL || 'mongodb://localhost:27017/graphql-example',
   tokenKey: 'secretKey'
 }
--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,25 +1,4 @@
-const { PassThrough } = require('stream');
-
 export function errorHandler(err, req, res, next) {
   const { status = 500, message = 'Server error' } = err;
   res.status(status).send({ message });
-}
-
-export function graphqlMiddlewareWrapper(graphqlMiddleware) {
-  return (req, res, next) => {
-    const resProxy = new PassThrough();
-    resProxy.headers = new Map();
-    resProxy.statusCode = 200;
-    resProxy.setHeader = (name, value) => {
-      resProxy.headers.set(name, value);
-    };
-    res.graphqlResponse = (cb) => {
-      res.statusCode = resProxy.statusCode;
-      resProxy.headers.forEach((value, name) => {
-        res.setHeader(name, value);
-      });
-      resProxy.pipe(res).on('finish', cb);
-    };
-    graphqlMiddleware(req, resProxy).then(() => next(), next);
-  };
 }
